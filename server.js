b3973e58d49c18575dacbaf0dc5602bae746f2ea
Isabelle Lindberg <h24islin@du.se>
2026-01-07 11:19:08 +0100

    json

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const port = process.env.PORT || 3000;
-const express = require('express');
+const express = require("express");
 const server = express();
-const sqlite3 = require('sqlite3');
+const sqlite3 = require("sqlite3");
 
 server.listen(port);
